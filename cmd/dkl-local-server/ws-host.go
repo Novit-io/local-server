0ed062a7ff16b72cec625ad9543b9cda362ea10c
Mikaël Cluseau <mcluseau@isi.nc>
2019-02-04 15:11:11 +1100

    cleanup

--- cmd/dkl-local-server/ws-host.go
+++ cmd/dkl-local-server/ws-host.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"net/http"
 	"path"
 
 	restful "github.com/emicklei/go-restful"
 
 	"novit.nc/direktil/local-server/pkg/mime"
+	"novit.nc/direktil/pkg/localconfig"
 )
+
+var trustXFF = flag.Bool("trust-xff", true, "Trust the X-Forwarded-For header")
 
 type wsHost struct {
 	prefix  string
@@ -89,4 +94,64 @@ func (ws *wsHost) render(req *restful.Request, resp *restful.Response) {
 	what := path.Base(req.Request.URL.Path)
 
 	renderHost(resp.ResponseWriter, req.Request, what, host, cfg)
+}
+
+func renderHost(w http.ResponseWriter, r *http.Request, what string, host *localconfig.Host, cfg *localconfig.Config) {
+	ctx, err := newRenderContext(host, cfg)
+	if err != nil {
+		log.Printf("host %s: %s: failed to render: %v", what, host.Name, err)
+		http.Error(w, "", http.StatusServiceUnavailable)
+		return
+	}
+
+	switch what {
+	case "ipxe":
+		w.Header().Set("Content-Type", "text/x-ipxe")
+	case "config":
+		w.Header().Set("Content-Type", "text/vnd.yaml")
+	default:
+		w.Header().Set("Content-Type", "application/octet-stream")
+	}
+
+	switch what {
+	case "ipxe":
+		err = renderIPXE(w, ctx)
+
+	case "kernel":
+		err = renderKernel(w, r, ctx)
+
+	case "initrd":
+		err = renderCtx(w, r, ctx, what, buildInitrd)
+
+	case "boot.iso":
+		err = renderCtx(w, r, ctx, what, buildBootISO)
+
+	case "boot.tar":
+		err = renderCtx(w, r, ctx, what, buildBootTar)
+
+	case "boot.img":
+		err = renderCtx(w, r, ctx, what, buildBootImg)
+
+	case "boot.img.gz":
+		err = renderCtx(w, r, ctx, what, buildBootImgGZ)
+
+	case "boot.img.lz4":
+		err = renderCtx(w, r, ctx, what, buildBootImgLZ4)
+
+	case "config":
+		err = renderConfig(w, r, ctx)
+
+	default:
+		http.NotFound(w, r)
+	}
+
+	if err != nil {
+		if isNotFound(err) {
+			log.Printf("host %s: %s: %v", what, host.Name, err)
+			http.NotFound(w, r)
+		} else {
+			log.Printf("host %s: %s: failed to render: %v", what, host.Name, err)
+			http.Error(w, "", http.StatusServiceUnavailable)
+		}
+	}
 }
