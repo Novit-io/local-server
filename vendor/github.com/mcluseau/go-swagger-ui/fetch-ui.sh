12cfc865604d6fd684fa0c20a26bfa18ecb8d5aa
Mikaël Cluseau <mcluseau@isi.nc>
2019-02-04 14:03:59 +1100

    update swagger-ui

--- vendor/github.com/mcluseau/go-swagger-ui/fetch-ui.sh
+++ vendor/github.com/mcluseau/go-swagger-ui/fetch-ui.sh
@@ -4,10 +4,12 @@ v=3.20.6
 
 file=v$v.tar.gz
 
+set -ex
+
 [ -e swagger-ui/$file ] ||
     wget -P swagger-ui https://github.com/swagger-api/swagger-ui/archive/$file
 
 rm -fr swagger-ui/dist
 tar zxvf swagger-ui/$file --strip-components=1 -C swagger-ui swagger-ui-$v/dist
 
-sed -i -e '/url:/s,https.*/,/,' swagger-ui/dist/index.html
+sed -i -e '/url:/s,https://petstore.swagger.io/v2/swagger.json,/swagger.json,g' swagger-ui/dist/*
